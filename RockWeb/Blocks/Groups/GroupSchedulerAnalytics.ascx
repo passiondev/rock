e76f2fe7aba8ff8943d512bc292a1707f9ab4841
ethan-sparkdevnetwork <ethan@sparkdevnetwork.org>
2019-02-12 16:48:00 -0700

    - EOD Check-in, testing chart.js

--- RockWeb/Blocks/Groups/GroupSchedulerAnalytics.ascx
+++ RockWeb/Blocks/Groups/GroupSchedulerAnalytics.ascx
@@ -12,20 +12,21 @@
             </div>
 
             <div class="panel-body">
-                <div class="row row-eq-height">
+                <div class="row">
                     <div class="col-lg-2 col-md-3 filter-options">
                         <Rock:NotificationBox ID="nbFilterNotification" runat="server" NotificationBoxType="Warning" visible="false"></Rock:NotificationBox>
 
                         <label>Please select a Group, Person, or Data View</label>
                             
                         <%-- Group picker --%>
-                        <Rock:GroupPicker ID="gpGroups" runat="server" AllowMultiSelect="false" Label="Select Groups" LimitToSchedulingEnabledGroups="true" OnSelectItem="gpGroups_SelectItem" />
+                        <Rock:GroupPicker ID="gpGroups" runat="server" AllowMultiSelect="false" Label="Select Groups" LimitToSchedulingEnabledGroups="true" OnSelectItem="gpGroups_SelectItem"  />
 
                         <%-- Person Picker --%>
                         <Rock:PersonPicker ID="ppPerson" runat="server" Label="Person" OnSelectPerson="ppPerson_SelectPerson" />
                         
                         <%-- Date Picker --%>
-                        <Rock:SlidingDateRangePicker ID="sdrpDateRange" runat="server" Label="Date Range" AllowPastDateSelection="true" AllowFutureDateSelection="true" />
+                        <Rock:SlidingDateRangePicker ID="sdrpDateRange" runat="server" Label="Date Range" AllowPastDateSelection="true" AllowFutureDateSelection="true"
+                            EnabledSlidingDateRangeTypes="Previous, Last, Current, DateRange" SlidingDateRangeMode="Current" EnabledSlidingDateRangeUnits="Week, Month, Year" />
 
                         <%-- Locations CBL --%>
                         <Rock:RockCheckBoxList ID="cblLocations" runat="server" Label="Locations" RepeatColumns="1" RepeatDirection="Vertical" OnSelectedIndexChanged="cblLocations_SelectedIndexChanged" AutoPostBack="true" ></Rock:RockCheckBoxList>
@@ -44,7 +45,7 @@
                         <div class="row">
                             <%-- Bar chart to show the data in the tabular --%>
                             <div class="chart-container">
-                            <Rock:NotificationBox ID="nbBarChartMessage" runat="server" NotificationBoxType="Info" Text="No Group Scheduler Data To Show" />
+                            <Rock:NotificationBox ID="nbBarChartMessage" runat="server" NotificationBoxType="Info" Text="No Group Scheduler Data To Show" visible="false"/>
                             <canvas id="barChartCanvas" runat="server" style="height: 450px;" />
                         </div>
 
@@ -58,10 +59,13 @@
                             <%-- tabular data --%>
                             <Rock:Grid ID="gData" runat="server" AllowPaging="true" EmptyDataText="No Data Found" ShowActionsInHeader="false">
                                 <Columns>
-                                    <Rock:RockBoundField DataField="FullName" HeaderText="Name"></Rock:RockBoundField>
-                                    <Rock:RockBoundField DataField="CheckIns" HeaderText="Checked In"></Rock:RockBoundField>
-                                    <Rock:RockBoundField DataField="MissedConfirmations" HeaderText="Missed Confirmations"></Rock:RockBoundField>
+                                    <Rock:RockBoundField DataField="Name" HeaderText="Name"></Rock:RockBoundField>
+                                    <Rock:RockBoundField DataField="Scheduled" HeaderText="Scheduled"></Rock:RockBoundField>
+                                    <Rock:RockBoundField DataField="NoResponse" HeaderText="NoResponse"></Rock:RockBoundField>
                                     <Rock:RockBoundField DataField="Declines" HeaderText="Declines"></Rock:RockBoundField>
+                                    <Rock:RockBoundField DataField="Attended" HeaderText="Attended"></Rock:RockBoundField>
+                                    <Rock:RockBoundField DataField="CommitedNoShow" HeaderText="CommitedNoShow"></Rock:RockBoundField>
+                                    <Rock:RockBoundField DataField="TentativeNoShow" HeaderText="TentativeNoShow"></Rock:RockBoundField>
                                 </Columns>
                             </Rock:Grid>
 
@@ -70,32 +74,52 @@
                     </div>
                 </div>
             </div>
-
             <script>
-                Sys.Application.add_load(function () {
+                window.onload = function () {
+                    <%--
+                    var barChartScheduled = '[{\"Name\":\"02\",\"Count\":14}]';
+                    var barchartCtx = $('#<%=this.barChartCanvas.ClientID%>')[0].getContext('2d');
 
-                    var chartSeriesColors = <%=this.SeriesColorsJSON%>;
+                    var barChart = new Chart(barchartCtx, {
+                        type: 'bar',
+                        data: {
+                            datasets: [{
+                                xAxisID: 'Name',
+                                yAxisID: 'Count',
+                                label: 'Scheduled',
+                                data: barChartScheduled
+                            }],
 
-                    var getSeriesColors = function(numberOfColors) {
-
-                        var result = chartSeriesColors;
-                        while (result.length < numberOfColors)
-                        {
-                            result = result.concat(chartSeriesColors);
+                        },
+                        options: {
+                            title: {
+                                display: true,
+                                text: 'My Title'
+                            }
                         }
+                    });--%>
 
-                        return result;
-                    };
+                    var ctx = $('#<%=this.barChartCanvas.ClientID%>')[0].getContext('2d');
+                    var chart = new Chart(ctx, {
+                        // The type of chart we want to create
+                        type: 'bar',
 
+                        // The data for our dataset
+                        data: {
+                            labels: ["01", "02", "03", "04"],
+                            datasets: [{
+                                label: "My First dataset",
+                                backgroundColor: 'rgb(255, 99, 132)',
+                                borderColor: 'rgb(255, 99, 132)',
+                                data: [0,14,0],
+                            }]
+                        },
 
-
-
-
-
-
-                });
+                        // Configuration options go here
+                        options: {}
+                    });
+                };
             </script>
-
         </asp:Panel>
     </ContentTemplate>
 </asp:UpdatePanel>
--- RockWeb/Blocks/Groups/GroupSchedulerAnalytics.ascx.cs
+++ RockWeb/Blocks/Groups/GroupSchedulerAnalytics.ascx.cs
@@ -27,7 +27,9 @@ using Rock;
 using Rock.Attribute;
 using Rock.Data;
 using Rock.Model;
+using Rock.Web.Cache;
 using Rock.Web.UI;
+using Rock.Web.UI.Controls;
 
 namespace RockWeb.Blocks.Groups
 {
@@ -42,7 +44,25 @@ namespace RockWeb.Blocks.Groups
     public partial class GroupSchedulerAnalytics : RockBlock
     {
         #region Properties
+        protected List<Attendance> attendances = new List<Attendance>();
+
         public string SeriesColorsJSON { get; set; }
+
+        public string BarChartScheduledJSON { get; set; }
+        public string BarChartNoResponseJSON { get; set; }
+        public string BarChartDeclinesJSON { get; set; }
+        public string BarChartAttendedJSON { get; set; }
+        public string BarChartCommitedNoShowJSON { get; set; }
+        public string BarChartTentativeNoShowJSON { get; set; }
+
+        /// <summary>
+        /// Gets or sets the line chart time format. see http://momentjs.com/docs/#/displaying/format/
+        /// </summary>
+        /// <value>
+        /// The line chart time format.
+        /// </value>
+        public string BarChartTimeFormat { get; set; }
+
 
         #endregion Properties
 
@@ -61,8 +81,9 @@ namespace RockWeb.Blocks.Groups
             RockPage.AddScriptLink( "~/Scripts/Chartjs/Chart.js", true );
 
             // this event gets fired after block settings are updated. it's nice to repaint the screen if these settings would alter it
-            //this.BlockUpdated += Block_BlockUpdated;
-            //this.AddConfigurationUpdateTrigger( upnlContent );
+            this.BlockUpdated += Block_BlockUpdated;
+            this.AddConfigurationUpdateTrigger( upnlContent );
+
         }
 
         /// <summary>
@@ -73,13 +94,153 @@ namespace RockWeb.Blocks.Groups
         {
             base.OnLoad( e );
         
-            if( !Page.IsPostBack )
+            if( Page.IsPostBack )
             {
                 
             }
         }
 
         #endregion Overrides
+
+        protected void RegisterScript()
+        {
+            string script = string.Format( @"
+var barChartScheduled = '{0}';
+var barChartNoResponse = '{1}';
+var barChartDeclines = '{2}';
+var barChartAttended = '{3}';
+var barChartCommitedNoShow = '{4}';
+var barChartTentativeNoShow = '{5}';
+
+var barchartCtx = $('#{6}')[0].getContext('2d');
+
+var barChart = new Chart(barchartCtx, {{
+    type: 'bar',
+    data: {{
+        labels: ['1', '2', '3'],
+        datasets: [{{
+            xAxisID: 'Name',
+            yAxisID: 'Count',
+            label: 'Scheduled',
+            data: barChartScheduled
+        }}],
+
+    }},
+    options: {{
+        title: {{
+            display: true,
+            text: 'My Title'
+        }}
+    }}
+}});
+",
+            BarChartScheduledJSON,
+            BarChartNoResponseJSON,
+            BarChartDeclinesJSON,
+            BarChartAttendedJSON,
+            BarChartCommitedNoShowJSON,
+            BarChartTentativeNoShowJSON,
+            barChartCanvas.ClientID
+            );
+
+            if ( gpGroups.GroupId != null )
+            {
+                    ScriptManager.RegisterClientScriptBlock( this, this.GetType(), "chartScript", script, true );
+            }
+        }
+
+        /// <summary>
+        /// What to do if the block settings are changed.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void Block_BlockUpdated( object sender, EventArgs e )
+        {
+            this.NavigateToCurrentPageReference();
+        }
+
+        /// <summary>
+        /// Clears the existing data from class var attendances and repopulates it with data for the selected group and filter criteria.
+        /// Data is organized by each person in the group.
+        /// </summary>
+        private void GetAttendanceDataForGroup()
+        {
+            attendances.Clear();
+            // Source data for all tables and graphs
+            using ( var rockContext = new RockContext() )
+            {
+                var attendanceService = new AttendanceService( rockContext );
+                var groupAttendances = attendanceService
+                    .Queryable()
+                    .AsNoTracking()
+                    .Where( a => a.Occurrence.GroupId == gpGroups.GroupId );
+
+                if ( sdrpDateRange.DelimitedValues.IsNotNullOrWhiteSpace())
+                {
+                    var dateRange = SlidingDateRangePicker.CalculateDateRangeFromDelimitedValues( sdrpDateRange.DelimitedValues );
+                    // parse the date range and add to query
+                    if ( dateRange.Start.HasValue )
+                    {
+                        groupAttendances = groupAttendances.Where( a => DbFunctions.TruncateTime( a.StartDateTime ) >= dateRange.Start.Value );
+                    }
+
+                    if ( dateRange.End.HasValue )
+                    {
+                        groupAttendances = groupAttendances.Where( a => DbFunctions.TruncateTime( a.StartDateTime ) <= dateRange.End.Value );
+                    }
+                }
+
+                if ( cblLocations.SelectedValues.Any() )
+                {
+                    // add selected locations to the query
+                    groupAttendances = groupAttendances.Where( a => cblLocations.SelectedValuesAsInt.Contains( a.Occurrence.LocationId ?? -1 ) );
+                }
+
+                if (cblSchedules.SelectedValues.Any() )
+                {
+                    // add selected schedules to the query
+                    groupAttendances = groupAttendances.Where( a => cblSchedules.SelectedValuesAsInt.Contains( a.Occurrence.ScheduleId ?? -1 ) );
+                }
+
+                attendances = groupAttendances.ToList();
+            }
+        }
+
+        /// <summary>
+        /// Clears the existing data from class var attendances and repopulates it with data for the selected person and filter criteria.
+        /// </summary>
+        private void GetAttendanceDataForPerson()
+        {
+            // Source data for all tables and graphs
+            using ( var rockContext = new RockContext() )
+            {
+                var attendanceService = new AttendanceService( rockContext );
+                var groupAttendances = attendanceService
+                    .Queryable()
+                    .AsNoTracking()
+                    .Where( a => a.PersonAliasId == ppPerson.PersonAliasId );
+
+                if ( sdrpDateRange.DelimitedValues.IsNotNullOrWhiteSpace())
+                {
+                    // parse the date range and add to query
+                    // groupAttendances = groupAttendances.Where
+                }
+
+                if ( cblLocations.SelectedValues.Any() )
+                {
+                    // add selected locations to the query
+                    // groupAttendances = groupAttendances.Where
+                }
+
+                if (cblSchedules.SelectedValues.Any() )
+                {
+                    // add selected schedules to the query
+                    // groupAttendances = groupAttendances.Where
+                }
+
+                attendances = groupAttendances.ToList();
+            }
+        }
 
         /// <summary>
         /// Populates the locations checkbox list for the selected group
@@ -179,56 +340,150 @@ namespace RockWeb.Blocks.Groups
         }
 
 
-        protected void ShowBarGraph()
+        protected void ShowBarGraphForGroup()
+        {
+            if ( !attendances.Any() )
+            {
+                return;
+            }
+
+            List<SchedulerGroupMember> barchartdata = null;
+
+            this.SeriesColorsJSON = this.GetAttributeValue( "SeriesColors" ).SplitDelimitedValues().ToArray().ToJson();
+            this.BarChartTimeFormat = "LL";
+
+            DateTime firstDateTime;
+            DateTime lastDateTime;
+
+            if ( sdrpDateRange.DelimitedValues.IsNotNullOrWhiteSpace() )
+            {
+                var dateRange = SlidingDateRangePicker.CalculateDateRangeFromDelimitedValues( sdrpDateRange.DelimitedValues );
+                firstDateTime = dateRange.Start.Value;
+                lastDateTime = dateRange.End.Value;
+                var timeUnit = sdrpDateRange.TimeUnit;
+            }
+            else
+            {
+                firstDateTime = attendances.Min( a => a.StartDateTime );
+                lastDateTime = attendances.Max( a => a.StartDateTime );
+            }
+
+            TimeSpan roundTimeSpan = TimeSpan.FromDays( 1 );
+
+            //var daysCount = ( lastDateTime - firstDateTime ).TotalDays;
+            //string groupBy = string.Empty;
+
+            //if ( daysCount / 7 > 26 )
+            //{
+            //    // if more than 6 months summarize by month
+            //    groupBy = "MM";
+            //}
+            //else if ( daysCount > 30 )
+            //{
+            //    // if more than 1 month summarize by week
+            //}
+            //else if ( daysCount > 1 )
+            //{
+            //    // if more than 1 day summarize by day
+            //}
+            //else
+            //{
+            //    // For one day summarize by hour
+            //    this.BarChartTimeFormat = "h A";
+            //}
+
+            var weeksCount = ( lastDateTime - firstDateTime ).TotalDays / 7;
+
+            if ( weeksCount > 26 )
+            {
+                // if there is more than 26 weeks worth, summarize by week
+                roundTimeSpan = TimeSpan.FromDays( 7 );
+            }
+            else if ( weeksCount > 3 )
+            {
+                // if there is more than 3 weeks worth, summarize by day
+                roundTimeSpan = TimeSpan.FromDays( 1 );
+            }
+            else
+            {
+                // if there is less than 3 weeks worth, summarize by hour
+                roundTimeSpan = TimeSpan.FromHours( 1 );
+                this.BarChartTimeFormat = "LLLL";
+            }
+
+
+            barchartdata = attendances
+                //.GroupBy( a => new { roundedTime = a.Min( b => b.StartDateTime ).Round( roundTimeSpan ) } )
+                .GroupBy( a => new { StartTime = a.StartDateTime.ToString( "MM") } )
+                .Select( a => new SchedulerGroupMember
+                {
+                    //Name = System.Globalization.CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName( a.Key.StartTime.AsIntegerOrNull() ?? 1 ),
+                    Name = a.Key.StartTime,
+                    Scheduled = a.Count(),
+                    NoResponse = a.Count( aa => aa.RSVP == RSVP.Unknown ),
+                    Declines = a.Count( aa => aa.RSVP == RSVP.No ),
+                    Attended = a.Count( aa => aa.DidAttend == true ),
+                    CommitedNoShow = a.Count( aa => aa.RSVP == RSVP.Yes && aa.DidAttend == false ),
+                    TentativeNoShow = a.Count( aa => aa.RSVP == RSVP.Maybe && aa.DidAttend == false )
+                } )
+                .ToList();
+
+            barChartCanvas.Style[HtmlTextWriterStyle.Display] = barchartdata.Any() ? string.Empty : "none";
+            nbBarChartMessage.Visible = !barchartdata.Any();
+
+            BarChartScheduledJSON = barchartdata.Select( d => d.Scheduled ).ToJson();
+            BarChartNoResponseJSON = barchartdata.Select( d => d.NoResponse ).ToJson();
+            BarChartDeclinesJSON = barchartdata.Select( d => d.Declines ).ToJson();
+            BarChartAttendedJSON = barchartdata.Select( d => d.Attended ).ToJson();
+            BarChartCommitedNoShowJSON = barchartdata.Select( d => d.CommitedNoShow ).ToJson();
+            BarChartTentativeNoShowJSON = barchartdata.Select( d => d.TentativeNoShow ).ToJson();
+        }
+
+        protected void ShowBarGraphForPerson()
         {
 
         }
 
-        protected void ShowDoughnutGraph()
+        protected void ShowDoughnutGraphForGroup()
+        {
+            var declineGuid = DefinedTypeCache.Get( Rock.SystemGuid.DefinedType.VOLUNTEER_SCHEDULE_DECLINE_REASON );
+
+
+
+        }
+
+        protected void ShowDoughnutGraphForPerson()
         {
 
         }
 
-        protected void ShowTable()
-        {
-
-        }
-
-        protected void ShowGridForGroup()
+        protected void ShowGrid()
         {
             var schedulerGroupMembers = new List<SchedulerGroupMember>();
 
             using ( var rockContext = new RockContext() )
             {
-                var attendanceService = new AttendanceService( rockContext );
-                var attendances = attendanceService
-                    .Queryable()
-                    .AsNoTracking()
-                    .Where( a => a.Occurrence.GroupId == gpGroups.GroupId )
-                    .ToList();
+                var personAliasService = new PersonAliasService( rockContext );
 
-                
-                foreach ( var attendance in attendances )
+                foreach ( var personAliasId in attendances.Select( a => a.PersonAliasId ).Distinct() )
                 {
                     var schedulerGroupMember = new SchedulerGroupMember();
-                    // set the props here
-                    //SELECT PersonAliasId, COUNT(Id) AS Scheduled FROM #tmp GROUP BY PersonAliasId
-                    //SELECT PersonAliasId, COUNT(Id) AS NoResponse FROM #tmp WHERE [RSVP] = 3 GROUP BY PersonAliasId
-                    //SELECT PersonAliasId, COUNT(Id) AS Declines FROM #tmp WHERE [RSVP] = 0 GROUP BY PersonAliasId
-                    //SELECT PersonAliasId, COUNT(Id) AS [Attended] FROM #tmp WHERE [DidAttend] = 1 GROUP BY PersonAliasId
-                    //SELECT PersonAliasId, COUNT(Id) AS CommitedNoShow FROM #tmp WHERE [RSVP] = 1 AND [DidAttend] = 0 GROUP BY PersonAliasId
-                    //SELECT PersonAliasId, COUNT(Id) AS TentativeNoShow FROM #tmp WHERE [RSVP] = 2 AND [DidAttend] = 0 GROUP BY PersonAliasId
-
+                    schedulerGroupMember.Name = personAliasService.GetPerson( personAliasId.Value ).FullName;
+                    schedulerGroupMember.Scheduled = attendances.Where( a => a.PersonAliasId == personAliasId.Value ).Count();
+                    schedulerGroupMember.NoResponse = attendances.Where( a => a.PersonAliasId == personAliasId.Value && a.RSVP == RSVP.Unknown ).Count();
+                    schedulerGroupMember.Declines = attendances.Where( a => a.PersonAliasId == personAliasId.Value && a.RSVP == RSVP.No).Count();
+                    schedulerGroupMember.Attended = attendances.Where( a => a.PersonAliasId == personAliasId.Value && a.DidAttend == true ).Count();
+                    schedulerGroupMember.CommitedNoShow = attendances.Where( a => a.PersonAliasId == personAliasId.Value && a.RSVP == RSVP.Yes && a.DidAttend == false ).Count();
+                    schedulerGroupMember.TentativeNoShow = attendances.Where( a => a.PersonAliasId == personAliasId.Value && a.RSVP == RSVP.Maybe && a.DidAttend == false ).Count();
 
                     schedulerGroupMembers.Add( schedulerGroupMember );
                 }
-
             }
 
             gData.DataSource = schedulerGroupMembers;
             gData.DataBind();
         }
-
+        
         #region Control Events
 
         protected void gpGroups_SelectItem( object sender, EventArgs e )
@@ -244,9 +499,23 @@ namespace RockWeb.Blocks.Groups
 
         protected void btnUpdate_Click( object sender, EventArgs e )
         {
-            ShowBarGraph();
-            ShowDoughnutGraph();
-            ShowTable();
+            if ( gpGroups.GroupId != null )
+            {
+                GetAttendanceDataForGroup();
+                ShowGrid();
+                ShowBarGraphForGroup();
+                ShowDoughnutGraphForGroup();
+            }
+            else if ( ppPerson.PersonAliasId != null )
+            {
+                GetAttendanceDataForPerson();
+                ShowGrid();
+                ShowBarGraphForPerson();
+                ShowDoughnutGraphForPerson();
+            }
+
+            //RegisterScript();
+            // maybe put a warning here or make is so they can't click the button if they don't have a valid selection.
         }
 
         protected void cblLocations_SelectedIndexChanged( object sender, EventArgs e )
@@ -258,7 +527,8 @@ namespace RockWeb.Blocks.Groups
 
         protected class SchedulerGroupMember
         {
-            public Person person { get; set; }
+            public string Name { get; set; }
+            public string Key { get { return Name; } }
             public int Scheduled { get; set; }
             public int NoResponse { get; set; }
             public int Declines { get; set; }
